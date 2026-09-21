92bb731adcf6645048cc63f80e2cacc3118e7df4
Luke Snavely <lsnavely28@gmail.com>
2026-09-21 18:33:45 -0400

    Added a table to the UI so that the user can see a list of potential running songs. Also, added a play button. This allows users to preview songs. Need to work on UI design still

--- RunningApp/app.R
+++ RunningApp/app.R
@@ -9,12 +9,10 @@
 
 library(shiny)
 library(bslib) # For themes
+library(DT)
 
-# Making a pace table
-# pace_table <- data.frame(
-#   total_seconds = seq(240, 1200, by = 15) # 4 to 20 minutes by 15 seconds
-#   ideal_bpm = c()
-# )
+# Reading in top running songs
+songs <- read.csv("/Users/lukesnavely/Desktop/Capstone/Data/Top_100_Running_Songs.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -23,26 +21,32 @@ ui <- fluidPage(
   
   # Inputting height
   numericInput("height", "Height (inches):",
-               value = 0, min = 48, max = 84),
+               value = 70, min = 48, max = 84),
   
   # Inputting age
   numericInput("age", "Age (Years):",
-               value = 0, min = 10, max = 100),
+               value = 20, min = 10, max = 100),
   
   # Minutes and Seconds inputs
   tags$label("Pace Input", class = "control-label"),
   fluidRow(
-    column(2, numericInput("MINUTES", label = NULL, value = 0, min = 4, max = 20, step = 1)), # Minutes box
+    column(2, numericInput("MINUTES", label = NULL, value = 8, min = 5, max = 15, step = 1)), # Minutes box
     column(1, p("min", style = "margin-top: 8px; font-weight: bold;")), # Minutes title
-    column(2, numericInput("SECONDS", label = NULL, value = 0, min = 0, max = 59, step = 15)),
-    column(1, p("sec", style = "margin-top: 8px; font-weight: bold;"))
+    column(2, numericInput("SECONDS", label = NULL, value = 0, min = 0, max = 59, step = 15)), # Seconds box
+    column(1, p("sec", style = "margin-top: 8px; font-weight: bold;")) # Seconds title
   ),
   
   # Button to generate results
   actionButton("generate", "Generate Running Songs"),
 
   # Display estimated SPM
-  textOutput("estimated_spm")
+  textOutput("estimated_spm"),
+  
+  # Showing matched songs to calculated cadence
+  dataTableOutput("matching_songs"),
+  
+  # Spotify player
+  uiOutput("spotify_player")
 )
 
 server <- function(input, output, session) {
@@ -76,6 +80,7 @@ server <- function(input, output, session) {
       3.067 * speed_kmh()
   })
   
+  # This is the estimated cadence
   output$estimated_spm <- renderText({
     paste(
       "Estimated cadence: ",
@@ -84,7 +89,86 @@ server <- function(input, output, session) {
     )
   })
   
+  # Filtering for songs that match the BPM
+  matching_songs <- eventReactive(input$generate, {
+    
+    # BPM of the song
+    bpm <- 203.056 +
+      0.193 * input$age -
+      44.242 * height_m() +
+      3.067 * speed_kmh()
+
+    songs |>
+      dplyr::filter(Tempo <= bpm + 5, Tempo >= bpm - 5) |>
+      dplyr::select(
+        Track.URI,
+        Track.Name,
+        Artist.Name.s.,
+        Tempo
+      )
+  })
+  
+  # Create Spotify player
+  output$spotify_player <- renderUI({
+    
+    req(input$selected_song)
+    
+    # Convert Spotify URI:
+    # spotify:track:6epn3r7S14KUqlReYr77hA
+    # into:
+    # 6epn3r7S14KUqlReYr77hA
+    track_id <- sub(
+      "spotify:track:",
+      "",
+      input$selected_song
+    )
+    
+    tags$iframe(
+      src = paste0(
+        "https://open.spotify.com/embed/track/",
+        track_id
+      ),
+      width = "100%",
+      height = "152",
+      frameborder = "0",
+      allow = "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture",
+      loading = "lazy",
+      style = "border-radius: 12px; border: none;"
+    )
+  })
+  
+  # Display table
+  output$matching_songs <- renderDataTable({
+    
+    results <- matching_songs()
+    
+    # Create Play button for each song
+    results$Play <- paste0(
+      '<button class="spotify-button" ',
+      'onclick="Shiny.setInputValue(\'selected_song\', \'',
+      results$Track.URI,
+      '\', {priority: \'event\'})">',
+      '▶ Play',
+      '</button>'
+    )
+    
+    results |>
+      dplyr::select(
+        Track.Name,
+        Artist.Name.s.,
+        Tempo,
+        Play
+      )
+    
+  },
+  escape = FALSE,
+  selection = "none",
+  options = list(
+    pageLength = 10
+  ))
+
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
